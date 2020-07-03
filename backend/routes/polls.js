7ae7ba5837818b5c2187633f0ac165b773f00874
VijayJoshua02 <47939417+VijayJoshua02@users.noreply.github.com>
2020-07-03 16:07:05 +0530

    Add files via upload

--- backend/routes/polls.js
+++ backend/routes/polls.js
@@ -3,19 +3,30 @@ var Pusher = require('pusher');
 
 const router = express.Router()
 var pusher = new Pusher({
-    app_id: "1029572",
-    key: "a4645a38865120f41cec",
-    secret: "c7235045d339e20613e9",
-    cluster: "ap2",
-    encrypted: true
+    appId : "1029738",
+    key : "ae4e4ae9c1f3e842a401",
+    secret : "68e01218a30464d5dcca",
+    cluster : "ap2",
 });
 
 router.route('/').post((req, res, next) => {
-    console.log('triggering' , req.body.option)
     pusher.trigger('poll','vote', {
-        points: 10,
+        points: 1,
         option: req.body.option
     });
     res.send('Voted');
 });
+router.route('/timer').post((req,res) => {
+    if(req.body.timer === 'start'){
+        pusher.trigger('timer','start', {
+            timer: 'start'
+        });
+        res.send('started');
+    } else {
+        pusher.trigger('timer','start', {
+            timer: 'stop'
+        });
+    }
+    
+})
 module.exports = router;
