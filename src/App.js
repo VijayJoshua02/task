3bd440f26345a32850641796ffdc622872fcc5a9
VijayJoshua02 <47939417+VijayJoshua02@users.noreply.github.com>
2020-07-03 16:05:49 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Navbar from './components/navbar';
 import Poll from './components/poll';
 import Admin from './components/admin';
 function App() {
   return (
     <Router>
     <div className="container">
-      <Navbar/>
       <br/>
       <Route path="/" exact component={Poll}/>
       <Route path="/admin" component={Admin}/>
--- src/components/poll.js
+++ src/components/poll.js
@@ -1,105 +1,70 @@
 import React, { Component } from 'react';
 import axios from 'axios'
-import CanvasJSReact from '../canvasjs-2.3.2/canvasjs.react';
 import Pusher from 'pusher-js';
-import Admin from './admin';
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+let _this = null;
 export default class Poll extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedOption: ''
+            selectedOption: '',
+            btn_disabled: true
         }
     }
     handleOptionChange = (e) => {
         this.setState({
-            selectedOption: e.target.value
+            selectedOption: e.target.value,
         });
     }
+    timer = () => {
+        this.setState({
+            btn_disabled: false
+        })
+        var t = new Date()
+        t.setSeconds(t.getSeconds() + 30)
+        var x = setInterval(() => {
+            var now = new Date().getTime()
+            var distance = t.getTime() - now
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            document.getElementById("timer").innerHTML = minutes + ":" + seconds;
+            if (distance < 0) {
+                this.setState({
+                    btn_disabled: true
+                })
+                clearInterval(x);
+                document.getElementById("timer").innerHTML = "Poll Expired";
+            }
+        })
+      }
 
 
     onSubmit = (e) => {
         e.preventDefault()
-        let dataPoints = [
-            { label: "A", y: 0 },
-            { label: "B", y: 0 },
-            { label: "C", y: 0 },
-            { label: "D", y: 0 },
-        ]
-        const chartContainer = document.querySelector('#chartContainer');
-        
-        if (chartContainer) {
-            var chart = new CanvasJSChart.Chart("chartContainer",
-                {
-                    animationEnabled: true,
-                    theme: "theme2",
-                    data: [
-                        {
-                            type: "column",
-                            dataPoints: dataPoints
-                        }
-                    ]
-                });
-            chart.render();
-        }
-        Pusher.logToConsole = true
-
-        var pusher = new Pusher('a4645a38865120f41cec', {
-            cluster: 'ap2',
-            encrypted: true
-        });
-       
-        
-      
-        var channel = pusher.subscribe('poll');
-        
-        channel.bind_global(function(event, data) {
-            console.log(event)
-          dataPoints = dataPoints.map(x => {
-            if(x.label == data.framework) {
-              x.y += data.points;
-              return x
-            } else {
-              return x
-            }
-          });
-      
-        })
-
-
-        var app = {
-            polls: document.querySelectorAll('.radio-btn'),
-            options: ['A', 'B', 'C', 'D']
-        }
-
-        app.handlePollEvent = function (event, index) {
-            const option = this.options[index];
-            axios.post('http://localhost:7000/polls', { option: option })
-                .then((data) => {
-                    console.log(`Voted ${option}`)
-                })
-        }
-
-
-        app.setup = function () {
-            this.polls.forEach((option, index) => {
-                option.addEventListener('click', (event) => {
-                    this.handlePollEvent(event, index)
-                }, true)
+        axios.post('http://localhost:7000/polls', { option: this.state.selectedOption })
+            .then((data) => {
+                console.log(`Voted ${this.state.selectedOption}`)
             })
-        }
-
-        app.setup();
-
     }
-
+    componentDidMount = () => {
+        _this = this
+        var pusher = new Pusher('ae4e4ae9c1f3e842a401', {
+            cluster: 'ap2',
+          });
+          var channel = pusher.subscribe('timer')
+          channel.bind('start', function(data) {
+              _this.timer()
+          })
+        }
 
 
     render() {
         return (
             <div>
+                <div className="timer">
+                <p id='timer'>Timer</p>
+                </div>
                 <h5>Pick it!</h5>
-                <Admin message="data from polls"/>
+                {/* <Admin message="data from polls"/> */}
                 <form onSubmit={this.onSubmit}>
                     <div className="radio">
                         <label>
@@ -147,10 +112,9 @@ export default class Poll extends Component {
       </label>
                     </div>
                     <div className="form-group">
-                        <input type="submit" value="SUBMIT" className="btn btn-primary" />
+                        <input type="submit" value="SUBMIT" className="btn btn-primary" disabled={this.state.btn_disabled}/>
                     </div>
                 </form>
-                <script src="server.js"></script>
             </div>
 
         )
